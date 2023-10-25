220fca15be571e9ed10b41ed27b307efaeea62d5
Saco <threesking@gmail.com>
2023-10-25 17:40:00 +0800

    fix.

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def start_vm(vm_id):
         completed = subprocess.run(["qm", "start", f"{vm_id}"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, check=True)
         return f"{completed.stdout}"
     except Exception as err:
-        return f"{err.}"
+        return f"{err}"
